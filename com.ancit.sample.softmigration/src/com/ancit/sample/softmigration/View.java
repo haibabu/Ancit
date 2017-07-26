84e8ae31391ac44088c81e3bd3a0707e16c1fde7
PROJECT_2 <PROJECT_2@Babu-HP>
2017-07-26 21:46:21 +0530

    initial revision

---
+++ com.ancit.sample.softmigration/src/com/ancit/sample/softmigration/View.java
@@ -0,0 +1,66 @@
+package com.ancit.sample.softmigration;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+
+public class View  {
+	public static final String ID = "com.ancit.sample.softmigration.view";
+
+	private TableViewer viewer;
+	
+	@Inject
+	public View() {
+		// TODO Auto-generated constructor stub
+	}
+	private class StringLabelProvider extends ColumnLabelProvider {
+		@Override
+		public String getText(Object element) {
+			return super.getText(element);
+		}
+
+		@Override
+		public Image getImage(Object obj) {
+			return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+		}
+
+	}
+@PostConstruct
+	public void createPartControl(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.getTable().setLinesVisible(true);
+
+		TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
+		column.setLabelProvider(new StringLabelProvider());
+
+		viewer.getTable().getColumn(0).setWidth(200);
+		
+		viewer.setContentProvider(ArrayContentProvider.getInstance());
+		
+		// Provide the input to the ContentProvider
+		viewer.setInput(createInitialDataModel());
+	}
+
+
+	@Focus
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+	
+	private List<String> createInitialDataModel() {
+		return Arrays.asList("One", "Two", "Three");
+	}
+}
---
+++ com.ancit.sample.softmigration/src/com/ancit/sample/softmigration/ViewWrapper.java
@@ -0,0 +1,11 @@
+package com.ancit.sample.softmigration;
+
+import org.eclipse.e4.tools.compat.parts.DIViewPart;
+
+public class ViewWrapper extends DIViewPart {
+
+	public ViewWrapper() {
+		super(View.class);
+	}
+
+}
