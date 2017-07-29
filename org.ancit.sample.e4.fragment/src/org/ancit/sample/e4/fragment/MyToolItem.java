f77ded3500554158db47bf5535fbdeebb4e4cae1
PROJECT_2 <PROJECT_2@Babu-HP>
2017-07-29 22:05:27 +0530

    fragment sample added.

---
+++ org.ancit.sample.e4.fragment/src/org/ancit/sample/e4/fragment/MyToolItem.java
@@ -0,0 +1,12 @@
+ 
+package org.ancit.sample.e4.fragment;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class MyToolItem {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
---
+++ org.ancit.sample.e4.fragment/src/org/ancit/sample/e4/fragment/OpenHandler.java
@@ -0,0 +1,12 @@
+ 
+package org.ancit.sample.e4.fragment;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class OpenHandler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
